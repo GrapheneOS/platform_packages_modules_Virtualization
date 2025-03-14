3687a96004b0ee6e8f35c14e6a04cf1c6bbf4bb9
Keiichi Watanabe <keiichiw@google.com>
2025-03-14 22:21:18 +0900

    Terminal: Resize sparse disk when storage balloon is turned off
    When the storage balloon feature is enabled via an aconfig flag,
    the rootfs is converted to a sparse file, increasing its apparent
    size. When the flag is disabled again afterwards (e.g. rollback
    or local testing), the disk needs to be converted back to
    non-sparse. Otherwise, the disk resize settings screen would
    display its apparent size which is too big.
    
    This CL addresses this issue by shrinking the disk size with
    resize2fs if the rootfs is a sparse file while the storage balloon
    feature is disabled
    
    Bug: 382174138
    Test: enable 'terminal_storage_balloon' and disable it. Check disk
          size setting.
    
    Change-Id: Ifadaa709b8f74dc99b68c9088d6a4fda06805a98

--- android/TerminalApp/java/com/android/virtualization/terminal/MainActivity.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/MainActivity.kt
@@ -49,6 +49,7 @@ import androidx.activity.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.android.internal.annotations.VisibleForTesting
 import com.android.microdroid.test.common.DeviceProperties
+import com.android.virtualization.terminal.ErrorActivity.Companion.start
 import com.android.virtualization.terminal.VmLauncherService.VmLauncherServiceCallback
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -367,7 +368,7 @@ public class MainActivity :
                 )
                 .build()
 
-        val diskSize = intent.getLongExtra(EXTRA_DISK_SIZE, image.getSize())
+        val diskSize = intent.getLongExtra(EXTRA_DISK_SIZE, image.getApparentSize())
 
         val intent =
             VmLauncherService.getIntentForStart(
