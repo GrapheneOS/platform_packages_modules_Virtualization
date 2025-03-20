bb2919f236932225a962be0f8ca41632100bebfb
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-20 21:09:52 +0200

    Revert "fix logger initialization crash when old log file is present"
    This reverts commit 34cb1122c6e4242ac70ad60fbad93f7feeacf120.

--- android/TerminalApp/java/com/android/virtualization/terminal/Logger.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/Logger.kt
@@ -47,9 +47,6 @@ internal object Logger {
         }
 
         try {
-            if (Files.isRegularFile(dir)) {
-                Files.delete(dir)
-            }
             Files.createDirectories(dir)
             deleteOldLogs(dir, 10)
             val logPath = dir.resolve(LocalDateTime.now().toString() + ".txt")
