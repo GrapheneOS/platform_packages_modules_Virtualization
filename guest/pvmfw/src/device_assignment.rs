msft-mirror-aosp/platform.packages.modules.Virtualization

commit 474da80db1f28fbaec2f65566e30a297c418167d

pvmfw: Do not return error when interrupts property is not present
There can be some nodes without an interrupt property.
Do not return error in that case.

Test: m libpvmfw.device_assignment.test pvmfw
Bug: 385802423
Change-Id: I0ab048872d07220e3d54ea7995a24315f9fd5a31
Signed-off-by: Sreenad Menon <quic_sreemeno@quicinc.com>

guest/pvmfw/src/device_assignment.rs
@@ -742,7 +742,7 @@ struct AssignedDeviceInfo {
     // <reg> property from the crosvm DT
     reg: Vec<DeviceReg>,
     // <interrupts> property from the crosvm DT
-    interrupts: Vec<u8>,
+    interrupts: Option<Vec<u8>>,
     // Parsed <iommus> property from the crosvm DT. Tuple of PvIommu and vSID.
     iommus: Option<Vec<(PvIommu, Vsid)>>,
 }
@@ -796,19 +796,18 @@ impl AssignedDeviceInfo {
         Ok(())
     }
 
-    fn parse_interrupts(node: &FdtNode) -> Result<Vec<u8>> {
+    fn parse_interrupts(node: &FdtNode) -> Result<Option<Vec<u8>>> {
+        let Some(cells) = node.getprop_cells(c"interrupts")? else {
+            return Ok(None);
+        };
         // Validation: Validate if interrupts cell numbers are multiple of #interrupt-cells.
         // We can't know how many interrupts would exist.
-        let interrupts_cells = node
-            .getprop_cells(c"interrupts")?
-            .ok_or(DeviceAssignmentError::InvalidInterrupts)?
-            .count();
-        if interrupts_cells % CELLS_PER_INTERRUPT != 0 {
+        if cells.count() % CELLS_PER_INTERRUPT != 0 {
             return Err(DeviceAssignmentError::InvalidInterrupts);
         }
 
         // Once validated, keep the raw bytes so patch can be done with setprop()
-        Ok(node.getprop(c"interrupts").unwrap().unwrap().into())
+        Ok(Some(node.getprop(c"interrupts").unwrap().unwrap().into()))
     }
 
     // TODO(b/277993056): Also validate /__local_fixups__ to ensure that <iommus> has phandle.
@@ -914,7 +913,11 @@ impl AssignedDeviceInfo {
     fn patch(&self, fdt: &mut Fdt, pviommu_phandles: &BTreeMap<PvIommu, Phandle>) -> Result<()> {
         let mut dst = fdt.node_mut(&self.node_path)?.unwrap();
         dst.setprop(c"reg", &to_be_bytes(&self.reg))?;
-        dst.setprop(c"interrupts", &self.interrupts)?;
+        if let Some(interrupts) = &self.interrupts {
+            dst.setprop(c"interrupts", interrupts)?;
+        } else {
+            dst.nop_property(c"interrupts")?;
+        }
 
         if let Some(iommus) = &self.iommus {
             let mut iommus_vec = Vec::with_capacity(8 * iommus.len());
@@ -1345,7 +1348,7 @@ mod tests {
         let expected = [AssignedDeviceInfo {
             node_path: CString::new("/bus0/backlight").unwrap(),
             reg: vec![[0x9, 0xFF].into()],
-            interrupts: into_fdt_prop(vec![0x0, 0xF, 0x4]),
+            interrupts: Some(into_fdt_prop(vec![0x0, 0xF, 0x4])),
             iommus: Some(vec![]),
         }];
 
@@ -1370,7 +1373,7 @@ mod tests {
         let expected = [AssignedDeviceInfo {
             node_path: CString::new("/rng").unwrap(),
             reg: vec![[0x9, 0xFF].into()],
-            interrupts: into_fdt_prop(vec![0x0, 0xF, 0x4]),
+            interrupts: Some(into_fdt_prop(vec![0x0, 0xF, 0x4])),
             iommus: Some(vec![(PvIommu { id: 0x4 }, Vsid(0xFF0))]),
         }];
 

GrapheneOS/platform_packages_modules_Virtualization

commit 9c3f4455bcf02c781ad8045db37ceb769d6c4f4c

pvmfw: Fix panic on missing 'interrupts' in VM DT
Not all assignable nodes in the VM device tree may have an 'interrupts'
property.
If a malicious host adds this property via the VMM, attempting to remove
the non-existent 'interrupts' property from the VM DT can result in an
error.

This patch masks that error, as it's harmless.
It ensures that the final device tree reflects only what the VM DTBO
specifies, ignoring untrusted modifications from the host.

Test: Booted QTVM with pvmfw on Qualcomm SM8850 chipset.
Flag: EXEMPT b/406977494
Fixes: 474da80db1f2 ("pvmfw: Do not return error when interrupts property is not present")
Bug: 385802423
Bug: 422213097
Reviewed-on: https://partner-android-review.googlesource.com/3344578
Change-Id: I250513997d085aa35a5e69fc552cc58946d6fc8d
Signed-off-by: Sreenad Menon <quic_sreemeno@quicinc.com>

guest/pvmfw/src/device_assignment.rs
@@ -926,7 +926,10 @@ impl AssignedDeviceInfo {
         if let Some(interrupts) = &self.interrupts {
             dst.setprop(c"interrupts", interrupts)?;
         } else {
-            dst.nop_property(c"interrupts")?;
+            match dst.nop_property(c"interrupts") {
+                Err(FdtError::NotFound) => Ok(()),
+                r => r,
+            }?;
         }
 
         if let Some(iommus) = &self.iommus {