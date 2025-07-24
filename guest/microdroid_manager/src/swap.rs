GrapheneOS/platform_packages_modules_Virtualization

commit 0dc99d70d1f375dade73fc3d34420e3f3acfebfd

microdroid_manager: swap: Add support for selecting a compression algo
ZRAM supports various compression algorithms. We currently use the
default lzo-rle algorithm, but this allows for an easy way to change
the compression algorithm.

Bug: 433579841
Flag: EXEMPT ...
Change-Id: I3a33b2f3c22f7dd1053ace2885ee3ad0fa03088c
Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

guest/microdroid_manager/src/swap.rs
@@ -20,6 +20,7 @@ use std::io::{Error, Seek, SeekFrom, Write};
 use uuid::Uuid;
 
 const SWAP_DEV: &str = "block/zram0";
+const COMPRESSION_ALGORITHM: &str = "lzo-rle";
 
 /// Parse "MemTotal: N kB" from /proc/meminfo
 fn get_total_memory_kb() -> Result<u32> {
@@ -83,9 +84,22 @@ fn swapon(dev: &str) -> Result<()> {
     Ok(())
 }
 
+/// Selects a compression algorithm for ZRAM. Can fail if the compression algorithm is not
+/// supported by the kernel, which can be checked by reading /sys/$dev/comp_algorithm.
+fn select_compression_algorithm(dev: &str) -> Result<()> {
+    OpenOptions::new()
+        .read(false)
+        .write(true)
+        .open(format!("/sys/{}/comp_algorithm", dev))?
+        .write_all(COMPRESSION_ALGORITHM.as_bytes())?;
+    Ok(())
+}
+
 /// Turn on ZRAM-backed swap
 pub fn init_swap() -> Result<()> {
     let dev = SWAP_DEV;
+
+    select_compression_algorithm(dev)?;
 
     // Create a ZRAM block device the same size as total VM memory.
     let mem_kb = get_total_memory_kb()?;

commit ac9002a9c5d7234632c8786fdb6350f190a76ad6

microdroid_mgr/swap: Only enable desired compression algorithm if available
Commit 0dc99d70d1f3 ("microdroid_manager: swap: Add support for
selecting a compression algo") blindly attempts to select a compression
algorithm. This is not ideal, as the kernel in use may not support the
desired compression algorithm, in which case microdroid_manager will
fail to enable swap usage, and the system will refuse to boot.

Not supporting the desired compression algorithm is not the end of the
world. The system should be allowed to run without the specific
algorithm; it just may not perform as expected. Therefore, make the
compression algorihm selection best-effort (i.e. enable the desired
algorithm only if it's available).

Bug: 433840381
Flag: EXEMPT bugfix
Fixes: 0dc99d70d1f3 ("microdroid_manager: swap: Add support for selecting a compression algo")
Test: MicrodroidHostTestCases com.android.microdroid.test.MicrodroidHostTests#testDebuggerdBacktrace_protectedVm_false_os_microdroid
Change-Id: I7ea554f32b0690dda0b2d15033375ad31a0f245a
Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

guest/microdroid_manager/src/swap.rs
@@ -15,8 +15,9 @@
 //! Logic for configuring and enabling a ZRAM-backed swap device.
 
 use anyhow::{anyhow, Context, Result};
+use log::warn;
 use std::fs::{read_to_string, OpenOptions};
-use std::io::{Error, Seek, SeekFrom, Write};
+use std::io::{Error, Read, Seek, SeekFrom, Write};
 use uuid::Uuid;
 
 const SWAP_DEV: &str = "block/zram0";
@@ -84,14 +85,36 @@ fn swapon(dev: &str) -> Result<()> {
     Ok(())
 }
 
-/// Selects a compression algorithm for ZRAM. Can fail if the compression algorithm is not
-/// supported by the kernel, which can be checked by reading /sys/$dev/comp_algorithm.
+/// Selects a compression algorithm for ZRAM. Logs an error if the desired compression algorithm is
+/// not supported by the kernel, which can be checked by reading /sys/$dev/comp_algorithm.
 fn select_compression_algorithm(dev: &str) -> Result<()> {
-    OpenOptions::new()
-        .read(false)
-        .write(true)
-        .open(format!("/sys/{}/comp_algorithm", dev))?
-        .write_all(COMPRESSION_ALGORITHM.as_bytes())?;
+    let path = format!("/sys/{}/comp_algorithm", dev);
+    let mut f = OpenOptions::new().read(true).write(true).open(path)?;
+
+    // Read the list of available algorithms first.
+    let mut algorithms = String::new();
+    f.read_to_string(&mut algorithms)?;
+
+    // The format of the output of /sys/$dev/comp_algorithm is:
+    // algo_0 algo_1 [algo_2] ... algo_N-1
+    // where the algorithm enclosed by square brackets is the current algorithm.
+    let mut iter = algorithms.split_whitespace();
+    let brackets: &[_] = &['[', ']'];
+
+    for x in &mut iter {
+        let algorithm = x.trim_matches(brackets);
+        if algorithm == COMPRESSION_ALGORITHM {
+            // Only write to the file if the algorithm we want isn't the current algorithm.
+            if !x.starts_with('[') {
+                f.write_all(COMPRESSION_ALGORITHM.as_bytes())?;
+            }
+            return Ok(());
+        }
+    }
+
+    // If we make it to here, then the algorithm we wanted is not available, so log an error, and
+    // move on, since this is a non-fatal condition.
+    warn!("Requested compression algorithm not available; saw: {algorithms}");
     Ok(())
 }
 